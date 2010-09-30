c59a051fa96f75b625804f52660588c865ab47e7
Nicholas <nicholas@ghostmonk.com>
2010-09-30 07:05:07 -0400

    Circular buffer

---
+++ CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CircularBuffer
+{
+    class CircularBuffer
+    {
+        private List<int> data;
+        private int head;
+        private int tail;
+        private int capacity;
+
+        public CircularBuffer( int length )
+        {
+            capacity = length;
+            data = new List<int>( length );
+            head = 0;
+            tail = 0;
+        }
+        
+        public bool Store( int value )
+        {
+            if( BufferFull() ) return false;
+            data[ tail++ ] = value;
+            if( tail == capacity ) tail = 0;
+            return true;
+        }
+
+        public bool BufferFull()
+        {
+            if( tail + 1 == head ) return true;
+            if( tail == ( data.Count - 1 ) && head == 0 ) return true;
+            return false;
+        }
+
+        public int? Read()
+        {
+            if( head == tail ) return null;
+            int value = data[ head++ ];
+            if( head == data.Count ) head = 0;
+            return value;
+        }
+    }
+}
---
+++ CircularBuffer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircularBuffer
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            CircularBuffer circularBuffer = new CircularBuffer( 8 );
+
+            Console.WriteLine( "Storing 1" );
+            circularBuffer.Store( 1 );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Storing 2" );
+            circularBuffer.Store( 2 );
+            Console.WriteLine( "Storing 3" );
+            circularBuffer.Store( 3 );
+            Console.WriteLine( "Storing 4" );
+            circularBuffer.Store( 4 );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Storing 5" );
+            circularBuffer.Store( 5 );
+            Console.WriteLine( "Storing 6" );
+            circularBuffer.Store( 6 );
+            Console.WriteLine( "Storing 7" );
+            circularBuffer.Store( 7 );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Storing 8" );
+            circularBuffer.Store( 8 );
+            Console.WriteLine( "Storing 9" );
+            circularBuffer.Store( 9 );
+            Console.WriteLine( "Storing 10" );
+            circularBuffer.Store( 10 );
+            Console.WriteLine( "Storing 11" );
+            circularBuffer.Store( 11 );
+            Console.WriteLine( "Storing 12" );
+            circularBuffer.Store( 12 );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.WriteLine( "Reading: {0}", circularBuffer.Read() );
+            Console.ReadLine();
+        }
+    }
+}
